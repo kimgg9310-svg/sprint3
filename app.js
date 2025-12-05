b275fdd5836461cdc0a5398405572ffc32eb9271
김관규 <kimgg9310@naver.com>
2025-12-05 04:21:39 +0000

    try

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
-import productsRouter from "./routers/productsRouter.js";
-import commentsRouter from "./routers/commentsRouter.js";
-import imagesRouter from "./routers/imagesRouter.js";
-import {
-  defaultNotFoundHandler,
-  globalErrorHandler,
-} from "./controllers/errorController.js";
 import { Console } from "console";
 
 const app = express();
@@ -15,14 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("articles", articlesRouter);
-app.use("/products", productsRouter);
-app.use("/comments", commentsRouter);
-app.use("/images", imagesRouter);
-
-app.use(defaultNotFoundHandler);
-app.use(globalErrorHandler);
-
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+app.listen(3000, () => {
+  console.log(`server started on port 3000`);
 });
